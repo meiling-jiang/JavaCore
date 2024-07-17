2059c27f8be0d781a95bd25e628306e0f6995d50
MEILING JIANG <mljiang0427@gmail.com>
2024-07-17 15:45:19 -0400

    first commit

---
+++ src/main/java/org/example/day1/Day1note.java
@@ -0,0 +1,19 @@
+package org.example.day1;
+
+public class Day1note {
+    public static void main(String[] args) {
+        /*
+        1. maven
+            folder structure
+            types of repository: local(.m2), central(online search), remote(ask for password to get)
+            maven build lifecycle: clean, validate, compile ...
+            command line: mvn clean, mvn install, mvn verify ...
+
+        2. git: distribute version control
+
+
+
+         */
+
+    }
+}
